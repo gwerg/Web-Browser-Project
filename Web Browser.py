02ee8d6c83cfdadbd5c01455a654c25cb50b246f
gwerg <dornala.shaury@gmail.com>
2026-06-18 14:08:32 +0800

    Multiple search terms

--- Web Browser.py
+++ Web Browser.py
@@ -1,14 +1,20 @@
 # List of movie titles
 movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Forrest Gump", "Inception", "The Matrix", "Avengers: Infinity War", "Back to the Future", "The Lion King", "Pulp Fiction"]
 
-# Ask the user for a search term
-search_movie = input("Enter a movie title to search for: ")
+# Ask the user for search terms
+search_input = input("Enter movie title(s) to search for (separate multiple terms with spaces): ")
+
+# Split the input into individual search terms
+search_terms = search_input.split()
 
 matches = []
 
 for movie in movies:
-    if search_movie.lower() in movie.lower():
-        matches.append(movie)
+    # Check if any of the search terms are in the movie title
+    for term in search_terms:
+        if term.lower() in movie.lower():
+            matches.append(movie)
+            break  
 print("Search results:")
 if matches:
     for match in matches:
